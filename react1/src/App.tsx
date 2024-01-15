60146c1b37a741ebcf3528c9a8091b4866034279
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-15 15:27:25 +0530

    React Alert

--- react1/src/App.tsx
+++ react1/src/App.tsx
@@ -1,13 +1,21 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-	let items = ["C1", "C2", "C3"];
+  // let items = ["C1", "C2", "C3"];
 
-	const handleItemSelect = (item: string) => {
-		console.log(item);
-	}
+  // const handleItemSelect = (item: string) => {
+  // 	console.log(item);
+  // }
 
-	return <div><ListGroup items={items} heading="Cs" onItemSelect={handleItemSelect}/></div>
+  // return <div><ListGroup items={items} heading="Cs" onItemSelect={handleItemSelect}/></div>
+  return (
+    <div>
+      <Alert>
+        Hello <h1>World!</h1>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
